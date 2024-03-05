84be66ab9a23e3825ecafc69bacb893ae26a104c
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-03-05 09:41:48 +0100

    colors+assets

---
+++ my-project/src/App.jsx
@@ -0,0 +1,6 @@
+export default function App() {
+  return (
+      <h1 className="text-forground bg-background font-bold">Hello World!</h1>
+    
+  )
+}
---
+++ my-project/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+          forground: "hsl(var(--forground)/<alpha-value>)",
+          background: "hsl(var(--background)/<alpha-value>)",
+          top: "hsl(var(--top-background-pattern)/<alpha-value>)",
+          card: "hsl(var(--background-card)/<alpha-value>)",
+          hover: "hsl(var(--background-card-hover)/<alpha-value>)",
+          text: "hsl(var(--text)/<alpha-value>)",
+      }
+    },
+  },
+  plugins: [],
+}
